6a677774b42a14be8fcbd7a645534b8b5c8ae3eb
Vincent <vins@Pimentas-MacBook-Air.local>
2021-10-12 18:50:29 +0200

    update function ontasksubmit

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ function onTaskSubmit() {
     var div = document.createElement("div");
     var para = document.createElement("p");
     var inputValue = document.getElementById("myInput").value;
-    var txt = document.createTextNode(inputValue);
+    // var txt = document.createTextNode(inputValue);
     var tache = {
-        status: "to do",
+        status: "todo",
         value: inputValue
     }
+
     console.log(tache);
     arrayTaches.push(tache)
     console.log(arrayTaches)
+
+    var getInput = arrayTaches.map(function(tache) {
+
+        var txt = document.createTextNode(tache.value);
+        para.appendChild(txt);
+        document.getElementById("myUL").appendChild(div).appendChild(para);
+
+    })
     document.getElementById("myInput").value = ''
 
 
-    para.appendChild(txt);
-    if (inputValue === '') {
-        alert("You must write something!")
-    } else {
-        document.getElementById("myUL").appendChild(div).appendChild(para);
-    }
+    // para.appendChild(txt);
+    // if (inputValue === '') {
+    //     alert("You must write something!")
+    // } else {
+    //     document.getElementById("myUL").appendChild(div).appendChild(para);
+    // }
 }
