bac345607f88f8ae61c020e5224e5812075af279
Vincent <vins@Pimentas-MacBook-Air.local>
2021-10-12 15:58:52 +0200

    donné d'objet dans un tableau

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 // Fonction écrire une tâche
+var arrayTaches = []
 
 function onTaskSubmit() {
     var div = document.createElement("div");
     var inputValue = document.getElementById("myInput").value;
     var txt = document.createTextNode(inputValue);
+    var tache = {
+        status: "to do",
+        value: inputValue
+    }
+    console.log(tache);
+    arrayTaches.push(tache)
+    console.log(arrayTaches);
+    document.getElementById("myInput").value = ''
+
+
     div.appendChild(txt);
     if (inputValue === '') {
         alert("You must write something!");
     } else {
         document.getElementById("myUL").appendChild(div);
     }
-    document.getElementById("myInput").value = ""
-
 }
