f6241f7971cfe48af1d5a9182da844b4f0f5b615
Vincent <vins@Pimentas-MacBook-Air.local>
2021-10-13 14:40:45 +0200

    all

--- index.js
+++ index.js
@@ -46,3 +46,45 @@ function suprimeTask(i) {
         `
     })
 }
+
+
+
+// function random
+function randomtask() {
+    var taskRandom =["lundi boxe a 16h30", "jeudi faire les courses", " vendredi à 18h récupérer les enfant a l'école", "samedi soir soirée entres potes"]
+    var min = 0;
+    var max = 3;
+    var random = Math.floor(Math.random() * (max - min + 1) + min)
+    arrayTaches.push(taskRandom[random])
+
+    console.log(random);
+    console.log(taskRandom[random]);
+    console.log(arrayTaches);
+
+    var inputValue = document.getElementById("myInput").value;
+    var tache = {
+        status: "todo",
+        value: inputValue
+    }
+
+    console.log(tache);
+    arrayTaches.push(tache)
+    console.log(arrayTaches)
+
+    list.innerHTML = ''
+
+    arrayTaches.forEach(function(tache,index) {
+        list.innerHTML = list.innerHTML + `
+            <div class="div-task">
+                ${tache.value}
+                <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
+            </div>
+        `
+    })
+
+    document.getElementById("myInput").value = ''
+
+    
+
+   
+}
