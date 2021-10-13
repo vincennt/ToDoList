641831e2f0208f4f1d0a8c383b67efa6fc4dc89e
Alin Manole <alin.manole@yahoo.com>
2021-10-13 16:16:19 +0200

    edit js/html/css

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function onTaskSubmit() {
             <div class="div-task">
                 ${tache.value}
                 <div class="buttonDiv">
-                    <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
-                    <button onclick="newInput(${index})" class="button-modif">...</button>
+                <button onclick="newInput(${index})" class="button-modif">...</button>
+                <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
                 </div>
             </div>
         `
@@ -81,9 +81,12 @@ function randomtask() {
     arrayTaches.forEach(function(tache, index) {
         list.innerHTML = list.innerHTML + `
             <div class="div-task">
-                ${tache.value}
-                <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
-                <button onclick="newInput(${index})" class="button-modif" >Modifier</button>
+                    ${tache.value}
+                    <div class="buttonDiv">
+                        <button onclick="newInput(${index})" class="button-modif">...</button>
+                        <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
+                    </div>
+                </div>
             </div>
         `
     })
