b6c6358c447bd224914e0d79a11c4517308a3fb2
Nascimento95 <marco-pereira@hotmail.fr>
2021-10-13 12:16:19 +0200

    function delete task finish

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 // Fonction écrire une tâche
 var arrayTaches = []
+var list = document.getElementById("myUL")
 
 function onTaskSubmit() {
     var inputValue = document.getElementById("myInput").value;
-    var list = document.getElementById("myUL")
-
     var tache = {
         status: "todo",
         value: inputValue
@@ -16,14 +15,33 @@ function onTaskSubmit() {
 
     list.innerHTML = ''
 
-    arrayTaches.forEach(function(tache) {
+    arrayTaches.forEach(function(tache,index) {
         list.innerHTML = list.innerHTML + `
-            <div class="div-task">${tache.value}</div>
+            <div class="div-task">
+                ${tache.value}
+                <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
+            </div>
         `
     })
 
     document.getElementById("myInput").value = ''
+    
+}
 
+// function qui filtre le tableau et re affiche les nouvelle valeur du tableau après avoir suprimer la tache 
+function suprimeTask(i){
+    arrayTaches = arrayTaches.filter(function(value,index) {
+        return i !== index;
+    });
 
+    list.innerHTML = ''
 
+    arrayTaches.forEach(function(tache,index) {
+        list.innerHTML = list.innerHTML + `
+            <div class="div-task">
+                ${tache.value}
+                <button onclick="suprimeTask(${index})" class="button_suprim">X</button>
+            </div>
+        `
+    })
 }
